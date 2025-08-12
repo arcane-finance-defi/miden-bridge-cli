0xMiden/miden-client

Add TransactionSummary, AccountDelta, and BasicFungibleFaucet models to web client

crates/web-client/src/models/mod.rs
@@ -43,6 +43,7 @@ pub mod advice_map;
 pub mod assembler;
 pub mod asset_vault;
 pub mod auth_secret_key;
+pub mod basic_fungible_faucet;
 pub mod block_header;
 pub mod consumable_note_record;
 pub mod executed_transaction;
@@ -92,4 +93,5 @@ pub mod transaction_result;
 pub mod transaction_script;
 pub mod transaction_script_inputs;
 pub mod transaction_status;
+pub mod transaction_summary;
 pub mod word;

arcane-finance-defi/miden-bridge-cli

crates/web-client/src/models/mod.rs
@@ -35,23 +35,21 @@ pub mod account_delta;
 pub mod account_header;
 pub mod account_id;
 pub mod account_storage;
-pub mod account_storage_delta;
 pub mod account_storage_mode;
 pub mod account_storage_requirements;
 pub mod account_type;
-pub mod account_vault_delta;
 pub mod advice_inputs;
 pub mod advice_map;
 pub mod assembler;
 pub mod asset_vault;
 pub mod auth_secret_key;
+pub mod basic_fungible_faucet_component;
 pub mod block_header;
 pub mod consumable_note_record;
 pub mod executed_transaction;
 pub mod felt;
 pub mod foreign_account;
 pub mod fungible_asset;
-pub mod fungible_asset_delta;
 pub mod input_note;
 pub mod input_note_record;
 pub mod input_note_state;
@@ -96,4 +94,5 @@ pub mod transaction_result;
 pub mod transaction_script;
 pub mod transaction_script_inputs;
 pub mod transaction_status;
+pub mod transaction_summary;
 pub mod word;